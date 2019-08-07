f67b9657c03d398f643b51f13cd2666cfe9ac3a6
abhishek <abhishek@abhisheks-iMac.local>
2019-08-07 22:04:27 +0530

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public GameObject player;
+	public float speed;
+
+	public float offsetZ;
+	public float offsetX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	var forw = transform.forward;
+ 
+		//get forward in world coords
+		forw = transform.InverseTransformDirection(forw);
+		 
+		//remove the y component (up down rotation)
+		forw.y = 0;
+		forw.Normalize();
+		 
+		forw.x = player.transform.position.x + offsetX;
+		forw.z = player.transform.position.z + offsetZ;
+		forw.y = 4.48f;
+
+		//now append to global position, because forw is a unit vector, multiplying by translateZ makes the vector length = to translateZ
+		transform.position = forw; // * speed * -1;
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody rigidbody;
+
+	public float m_Speed;
+	public float m_Limit;
+
+	public bool isGoingForward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        isGoingForward = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (isGoingForward) {
+	    	//transform.Translate(Vector3.forward * -Time.deltaTime * m_Speed);
+	    	if (rigidbody.velocity.magnitude <= m_Limit)
+		    	rigidbody.AddForce(0, 0, -m_Speed);
+    	}
+	    else {
+	    	//transform.Translate(-Time.deltaTime * m_Speed , 0, 0);
+	    	if (rigidbody.velocity.magnitude <= m_Limit)
+		    	rigidbody.AddForce(-m_Speed, 0, 0);
+	    }
+
+	    Debug.Log ("Velocity is " + rigidbody.velocity.magnitude);
+
+    	 if (Input.GetMouseButtonDown(0)) {
+    		isGoingForward = !isGoingForward;
+    		//Debug.Log("isGoingForward is " + isGoingForward);
+    	 }
+    }
+}
